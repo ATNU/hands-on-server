19e7b29a33d961a4b30b00b4d03f7b93469c63de
Mark Turner <nmdt3@newhuaweiapstaff-10-56-194-97.ncl.ac.uk>
2019-12-04 14:53:48 +0000

    parameterized connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,17 @@ import { AppService } from './app.service';
 import { FeedbackModule } from './feedback/feedback.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
-
+/*
+* DB_HOST     = mongodb
+* DB_PORT     = 27001
+* DB_NAME     = hands-on-db
+* DB_USER     = 
+* DB_PASSWORD =
+*/
 
 @Module({
   imports: [
-      MongooseModule.forRoot('mongodb://mongo:27017/hands-on-db', { useNewUrlParser: true, useUnifiedTopology: true }),
+      MongooseModule.forRoot('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' +  + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME + '?ssl=true', { useNewUrlParser: true, useUnifiedTopology: true }),
       FeedbackModule],
   controllers: [AppController],
   providers: [AppService],
