38782a9ac76c4452a0b52cd0f404eb3b847bf8aa
KateCourt <nkc124@ncl.ac.uk>
2019-12-04 10:40:34 +0000

    update feedback schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-      MongooseModule.forRoot('mongodb://localhost:27017/hands-on-db', { useNewUrlParser: true, useUnifiedTopology: true }),
+      MongooseModule.forRoot('mongodb://mongo:27017/hands-on-db', { useNewUrlParser: true, useUnifiedTopology: true }),
       FeedbackModule],
   controllers: [AppController],
   providers: [AppService],
