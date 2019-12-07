9129447958475de2a9e6261f3b4f87781e7f2dfb
Mark Turner <nmdt3@18DCompL027.local>
2019-12-07 14:25:13 +0000

    added output check for connection string

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  console.info('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' +  + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME + '?ssl=true');
   await app.listen(3000);
 }
 bootstrap();
