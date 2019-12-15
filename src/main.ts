81ae1b4106c4e0d471269cddb1e7e172db04462f
Mark Turner <mark.turner@ncl.ac.uk>
2019-12-15 14:25:10 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  console.info('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' +  + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME + '?ssl=true');
+  console.info(process.env.DB_CONNECTION_STRING);
   await app.listen(3000);
 }
 bootstrap();
