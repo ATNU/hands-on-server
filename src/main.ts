fd8ba13d3711001e06292b7f8177dc05aac55cda
KateCourt <nkc124@ncl.ac.uk>
2020-02-03 11:59:24 +0000

    add try/catch to main.js

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  console.info(process.env.DB_CONNECTION_STRING);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.listen(3000);
+  } catch (err) {
+    console.log(err);
+    console.log(process.env.DB_CONNECTION_STRING);
+  }
 }
 bootstrap();
