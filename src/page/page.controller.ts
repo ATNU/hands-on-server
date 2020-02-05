9fa23448f41d497fef65cccf484ecb292b23db88
KateCourt <nkc124@ncl.ac.uk>
2020-02-05 15:20:44 +0000

    get furthest page for logged in user

--- src/page/page.controller.ts
+++ src/page/page.controller.ts
@@ -12,6 +12,7 @@ export class PageController {
         private pageService: PageService,
     ) {
     }
+
 // todo
     // get most recently saved version of page
     @Get()
@@ -19,16 +20,11 @@ export class PageController {
 
     }
 
-// todo
     @Post('save')
     async savePage(@Res() res, @Req() req, @Body() createPageDto: CreatePageDto) {
-        console.log('sent page ' + JSON.stringify(createPageDto));
 
-        if (mongoose.Types.ObjectId.isValid(req.body.jwt.id) ) {
-            console.log('invalid user ID');
+        if (mongoose.Types.ObjectId.isValid(req.body.jwt.id)) {
             const mongoUserId = mongoose.Types.ObjectId(req.body.jwt.id);
-
-            console.log('mgId ' + mongoUserId);
             const pageToSave = {
                 userId: mongoUserId,
                 pageNo: createPageDto.pageNo,
@@ -37,22 +33,53 @@ export class PageController {
                 timestamp: Date.now(),
             };
 
-            console.log('new page ' + JSON.stringify(pageToSave));
             const page = await this.pageService.save(pageToSave);
             return res.status(HttpStatus.CREATED).json({
                 message: 'Page saved',
             });
         }
-        }
-
-   // todo
-    @Get('resume')
-    async getMostRecentPage(@Res() res, @Req() req) {
-        console.log('page');
-        console.log(req.body.id);
     }
 
+    // return page object of the furthest saved page
+    @Get('furthestPage')
+    async getMostRecentPage(@Res() res, @Req() req) {
 
+        console.log('furthest page route');
+
+
+        // if there's no pages for this user then respond
+        const pagesForUser = await this.pageService.getAllForUser(req.body.jwt.id);
+        if (pagesForUser.length === 0) {
+            return res.status(HttpStatus.OK).json({
+                page: pagesForUser,
+                message: 'no saved pages',
+            });
+        }
+        
+        // get the number of the furthest page that user has saved
+        const highestPageNo = await this.pageService.getHighestPageNo(await this.pageService.getAllForUser(req.body.jwt.id));
+        console.log('highestPageNo ' + highestPageNo);
+        // get all saved pages for that pageNo
+        const savedPages = await this.pageService.getPagesForUserForPageNo(highestPageNo, req.body.jwt.id);
+        console.log('savedPages' + savedPages);
+
+        // if there's only 1 page return that
+        if (savedPages.length === 1 ) {
+            console.log('return single result');
+            return res.status(HttpStatus.OK).json({
+                page: savedPages[0],
+            });
+        } else {
+console.log('find most recent');
+            // find most recent page saved for the pageno
+            const mostRecent = await this.pageService.getMostRecentPage(savedPages);
+            console.log('mostRecent ' + mostRecent);
+            return res.status(HttpStatus.OK).json({
+                page: mostRecent,
+            });
+        }
+
+    }
 
 }
 
--- src/page/page.service.ts
+++ src/page/page.service.ts
@@ -1,9 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 import {InjectModel} from '@nestjs/mongoose';
 import {Page} from './page.interface';
 import {Model} from 'mongoose';
-import {CreateFeedbackDto} from '../feedback/createFeedback.dto';
-import {Feedback} from '../feedback/feedback.interface';
+import * as mongoose from 'mongoose';
 import {CreatePageDto} from './createPage.dto';
 
 @Injectable()
@@ -16,7 +15,105 @@ export class PageService {
         const savedPage = new this.pageModel(createPageDto);
         return await savedPage.save();
     }
+
     // todo get most recently saved version of page number x
 
+    async getAllForUser(userId) {
+        return await this.pageModel.find({userId});
+    }
 
+    async getPagesForUserForPageNo(pageNo, userId) {
+        return await this.pageModel.find({userId, pageNo});
+    }
+
+    async getMostRecentPage(pages) {
+        let highestTime = new Date('2020-02-05T14:03:40+00:00');
+        let mostRecentPage;
+        for (const pg of pages) {
+            const dateOfPg = new Date(pg.timestamp);
+
+            if (dateOfPg > highestTime) {
+                // new highestTime
+                highestTime = dateOfPg;
+                mostRecentPage = pg;
+            }
+        }
+        return mostRecentPage;
+    }
+
+//     async getFurthestPage(userId): Promise<Page> {
+//         console.log('furthest page in service');
+//         const allPages = await this.pageModel.find({userId});
+//
+// // if there's no pages then user has none saved
+//         if (allPages.length === 0) {
+//             console.log('no pages for this user');
+//             return null;
+//         } else {
+//             // if there's more than one page saved then look for most recent
+//             if (allPages.length > 1) {
+//
+//                 // for each page saved for that user find the highest pageNo
+//
+//                 const highestNumber = await this.getHighestPageNo(allPages);
+//
+//                 console.log('highestNumber ' + highestNumber);
+//
+//                 // get all pages with that pageNo
+//                 this.pageModel.find({userId, pageNo: highestNumber}).then((pagesForHighest) => {
+//                     console.log('pagesForHighest' + pagesForHighest);
+//
+//                     // if there's only one then return this
+//                     if (pagesForHighest.length === 1) {
+//                         console.log('only 1 page for the highest page number');
+//                         const pageId1 = pagesForHighest[0]._id;
+//                         this.pageModel.find({_id: pageId1}).then((result) => {
+//                                 console.log('returning result1 ' + result);
+//                                 return result;
+//                             },
+//                         );
+//                     } else {
+//                         // look for the most recent timestamp
+//                         let highestTime = new Date('2020-02-05T14:03:40+00:00');
+//                         let mostRecentPage;
+//                         for (const pg of pagesForHighest) {
+//                             const dateOfPg = new Date(pg.timestamp);
+//
+//                             if (dateOfPg > highestTime) {
+//                                 // new highestTime
+//                                 highestTime = dateOfPg;
+//                                 mostRecentPage = pg;
+//                             }
+//                         }
+//                         const pageId2 = mostRecentPage._id;
+//
+//                         this.pageModel.find({_id: pageId2}).then((result) => {
+//                             console.log('returning 2');
+//                             return result;
+//                         });
+//                     }
+//                 });
+//             }
+//
+//             // if there's only 1 page saved then return this
+//             if (allPages.length === 1) {
+//                 console.log('only 1 page saved for this user');
+//
+//                 const pageId = allPages[0]._id;
+//
+//                 return await this.pageModel.find({_id: pageId});
+//             }
+//         }
+//     }
+
+    // get highest page number from a list of pages
+    async getHighestPageNo(allPages) {
+        let highestNumber = 0;
+        for (const page of allPages) {
+            if (page.pageNo >= highestNumber) {
+                highestNumber = page.pageNo;
+            }
+        }
+        return highestNumber;
+    }
 }
