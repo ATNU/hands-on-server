2a938d801107fdcce33e7eb09cc51f1500eb18e9
KateCourt <nkc124@ncl.ac.uk>
2020-02-13 08:59:34 +0000

    trigger build

--- src/feedback/feedback.controller.ts
+++ src/feedback/feedback.controller.ts
@@ -8,7 +8,7 @@ export class FeedbackController {
 
     }
 
-    // save
+    // save feed
     @Post('save')
     async saveFeedback(@Res() res, @Body() createFeedbackDto: CreateFeedbackDto) {
         const feedback = await this.feedbackService.save(createFeedbackDto);
